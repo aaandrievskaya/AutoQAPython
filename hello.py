351b7a6157f03f6e263d1845ee69586c4394576e
aaandrievskaya <andrievskayaaa@yandex.ru>
2026-09-03 19:36:44 +0300

    Добавила действий в программу

--- hello.py
+++ hello.py
@@ -1,4 +1,15 @@
+import random
+
 k = int(input())
 for i in range(k):
-    print("Python environment is ready, DETKA")
+    print("Python is ready, baby")
     print("WOW, i'm using Git... ")
+a = random.randint(0,10)
+b = random.randint(0,10)
+sum = a+b
+print (f'{a}+{b}=...?')
+sum1 = int(input())
+if sum1==sum:
+    print('Great!')
+else:
+    print('Loser!')
