e548d539069614a131fc1a16c669c94ea679b9d8
aaandrievskaya <andrievskayaaa@yandex.ru>
2026-09-03 19:39:00 +0300

    Убрала ненужное

--- hello.py
+++ hello.py
@@ -1,9 +1,5 @@
 import random
 
-k = int(input())
-for i in range(k):
-    print("Python is ready, baby")
-    print("WOW, i'm using Git... ")
 a = random.randint(0,10)
 b = random.randint(0,10)
 sum = a+b
