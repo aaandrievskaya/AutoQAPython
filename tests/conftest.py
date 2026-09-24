e00fb8f867de8df8fdf2b021141175aa60f6ba60
aaandrievskaya <andrievskayaaa@yandex.ru>
2026-09-25 00:53:15 +0300

    Создала новые папки и пустые файлы как требует карточка 3.4 Также обновила описание в README Также прогнала black и flake8 и по совету нейронки создала файл для flake8, чтобы он не прогонял системные файлы и чтобы максимальная ширина символов была 88 символа

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,13 @@
 import pytest
 
+
 @pytest.fixture(scope="session")
 def library():
     print("\nSETUP module fixture-1")
     data_books = ["Преступление и наказание", "Отцы и дети", "Гордость и предубеждение"]
     yield data_books
     print("\nTEARDOWN module fixture-1")
+
 
 @pytest.fixture(scope="session")
 def new_book():
--- tests/test_parameterization.py
+++ tests/test_parameterization.py
@@ -1,8 +1,11 @@
 import pytest
 
-@pytest.mark.strings
-@pytest.mark.parametrize("word, expected_length", [("QA", 2), ("Python", 6), ("", 0), ("Автоматизация", 13)], 
-                         ids=["short_word", "average_word", "empty_string", "long_word"])
 
+@pytest.mark.for3_3
+@pytest.mark.parametrize(
+    "word, expected_length",
+    [("QA", 2), ("Python", 6), ("", 0), ("Автоматизация", 13)],
+    ids=["short_word", "average_word", "empty_string", "long_word"],
+)
 def test_word(word, expected_length):
     assert len(word) == expected_length
